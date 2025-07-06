2db3cc26f7e63b23616273e9593cfc344e9d77ac
priyanshupriyank04 <priyanshupriyank04@gmail.com>
2025-07-06 20:55:32 +0530

    chunk file updated and added

---
+++ chunk.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+#include "chunk.h"
+
+void initChunk(Chunk *chunk)
+{
+    chunk->count = 0;
+    chunk->capacity = 0;
+    chunk->code = NULL;
+}
+
+void writeChunk(Chunk *chunk, uint8_t byte)
+{
+    if (chunk->capacity < chunk->count + 1)
+    {
+        int oldcapacity = chunk->capacity;
+        chunk->capacity = GROW_CAPACITY(chunk->capacity);
+        chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldcapacity, chunk->capacity);
+    }
+
+    chunk->code[chunk->count] = byte;
+    chunk->count++;
+}
--- chunk.h
+++ chunk.h
@@ -1,10 +1,22 @@
 #ifndef clox_chunk_h
-#define clox_chunk_h 
+#define clox_chunk_h
 
-#include "common.h"     // tells the compiler to first look for it in the local project folder and <> tells to look for in the system's standard library
+#include "common.h" // tells the compiler to first look for it in the local project folder and <> tells to look for in the system's standard library
 
-typedef enum {      // create named number type using typedef which reduces task of writing enum everytime 
+typedef enum
+{ // create named number type using typedef which reduces task of writing enum everytime
     OP_RETURN,
 } Op_code;
+
+typedef struct
+{
+    int count;     // count of filled capacity
+    int capacity;  // count of total capacity
+    uint8_t *code; // dynamic array of storing dynamic number of instructions, once completely filled new arr of 2x size copy array and free old memory
+} Chunk;
+
+void initChunk(Chunk *chunk); // function to initialise a chunk which is dynamic and stores all the byte instructions
+
+void writeChunk(Chunk *chunk, uint8_t byte);
 
 #endif
