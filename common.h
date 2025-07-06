d08656f0eb98443c9075bef1d133f5e641e4e805
priyanshupriyank04 <priyanshupriyank04@gmail.com>
2025-07-06 18:57:40 +0530

    common header library added

---
+++ common.h
@@ -0,0 +1,9 @@
+#ifndef clox_common_h   // header guard for user defined libraries to ensure only once libraries are defined
+#define clox_common_h
+
+#include <stdbool.h>   // library for importing bool datatype
+#include <stddef.h>    // for null pointers and counting size safely
+#include <stdint.h>     // for consistent int datatype throughout the program
+
+#endif  // end of header guard
+
