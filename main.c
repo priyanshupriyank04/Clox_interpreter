9232ffedc94aeb90a573de0cb6d9dd4e5bb301d6
priyanshupriyank04 <priyanshupriyank04@gmail.com>
2025-07-07 16:23:44 +0530

    files added

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
-#include <common.h>
+#include "common.h"
+#include "chunk.h"
 
-int main(int argc, const char* argv[]) // argc - argument counts and argv - actual text passed in each argument 
+int main(int argc, const char *argv[]) // argc - argument counts and argv - actual text passed in each argument
 {
+    Chunk chunk; // chunk here is the user defined container where we are storing the dynamic array of bytecode instructions
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+    freeChunk(&chunk);
+
     return 0;
 }
