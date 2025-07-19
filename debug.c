a45c0f5f22605f052d88a64318c4920ee967e9d4
priyanshupriyank04 <priyanshupriyank04@gmail.com>
2025-07-19 05:58:47 +0530

    files updated

---
+++ debug.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+#include "debug.h"
+
+void disassembleChunk(Chunk *chunk, const char *name)
+{
+    printf("%s\n", name); // prints chunk name
+
+    for (int offset = 0; offset < chunk->count;)
+    {
+        offset = disassembleInstruction(chunk, offset);
+    }
+}
+
+int disassembleInstruction(Chunk *chunk, int offset)
+{
+    printf("%04d", offset); // prints offset till 4 digits
+
+    uint8_t instruction = chunk->code[offset]; // stores unsigned 8 bit integer type instruction code using offset
+
+    switch (instruction)
+    {
+    case OP_RETURN:
+        return simpleInstruction("OP_RETURN", offset);
+
+    default:
+        printf("Unknown opcode %d\n", instruction);
+        return offset + 1;
+    }
+}
+
+static int simpleInstruction(const char *name, int offset) // handles checking and printing op return bytecode instruction for every 1-byte
+{
+    printf("%s\n", name);
+    return offset + 1;
+}
--- debug.h
+++ debug.h
@@ -4,6 +4,6 @@
 #include "chunk.h"
 
 void dsassembleChunk(Chunk* chunk, const char* name);
-int disassembleChunk(Chunk* chunk, int offset); //helps disassemble one line at a time, returns integer offset of next instruction in bytecode stored in chunk 
+int disassembleInstruction(Chunk* chunk, int offset); //helps disassemble one line at a time, returns integer offset of next instruction in bytecode stored in chunk 
 
 #endif
