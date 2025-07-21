defa02b1171ff2e1afbc8acf003fbbfe1706e3ea
priyanshupriyank04 <priyanshupriyank04@gmail.com>
2025-07-21 13:11:35 +0530

    vm initialised

--- main.c
+++ main.c
@@ -1,16 +1,27 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char *argv[]) // argc - argument counts and argv - actual text passed in each argument
 {
+    initVM();
+
     Chunk chunk; // chunk here is the user defined container where we are storing the dynamic array of bytecode instructions and //function which checks whole chunk.code array and converts the human readable format of the bytecode
+
     initChunk(&chunk);
-    int constant = addConstant(&chunk, 1.2); //manually initialising the double constant to add
-    writeChunk(&chunk,OP_CONSTANT,123); //manually adding op code to the chunk data pool
-    writeChunk(&chunk,constant,123);    //manually adding the constant to the chunk data pool after the op code
-    writeChunk(&chunk, OP_RETURN,123);
+
+    int constant = addConstant(&chunk, 1.2); // manually initialising the double constant to add
+
+    writeChunk(&chunk, OP_CONSTANT, 123); // manually adding op code to the chunk data pool
+
+    writeChunk(&chunk, constant, 123); // manually adding the constant to the chunk data pool after the op code
+
+    writeChunk(&chunk, OP_RETURN, 123);
+
     disassembleChunk(&chunk, "test chunk");
+
+    freeVM();
     freeChunk(&chunk);
 
     return 0;
