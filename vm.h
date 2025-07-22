3c5ea096ea1e38f8f42fe7e4c5aa9df24f9f98b7
priyanshupriyank04 <priyanshupriyank04@gmail.com>
2025-07-22 11:12:12 +0530

    initial vm updated

--- vm.h
+++ vm.h
@@ -8,7 +8,17 @@ typedef struct
     Chunk *chunk; // chunk is included because VM would be running and executing these bytecode instructions i.e. Chunk
 } VM;
 
+typedef enum
+{
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
+}InterpretResult;
+
+
 void initVM(); // sets up the VM to run code
 void freeVM(); // resets the VM
+
+InterpretResult interpret(Chunk* chunk);
 
 #endif
