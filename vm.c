d3ea002887f1ea76ab73ab3948c8c788b3251936
priyanshupriyank04 <priyanshupriyank04@gmail.com>
2025-07-27 23:34:46 +0530

    vm updated

--- vm.c
+++ vm.c
@@ -12,3 +12,29 @@ void freeVM()
 {
     
 }
+
+static InterpretResult run()
+{
+    #define READ_BYTE() (* vm.ip++) //function to keep track and update instruction pointer/program counter
+
+    for(;;)
+    {
+        uint8_t instruction;    //single bytecode instruction line
+        switch(instruction = READ_BYTE())
+        {
+            case OP_RETURN: //op return is the code for ending the program 
+            {   
+                return INTERPRET_OK;    //exits the program 
+            }
+        }
+    }
+
+    #undef READ_BYTE    //undefines the macro to ensure no program conflict
+}
+
+InterpretResult interpret(Chunk* chunk)
+{
+    vm.chunk = chunk;   // stores the bytecode instruction
+    vm.ip = vm.chunk->code; // instruction pointer keeps track of code being executed
+    return run();   // helper function which helps running bytecode instructions
+}
--- vm.h
+++ vm.h
@@ -6,6 +6,7 @@
 typedef struct
 {
     Chunk *chunk; // chunk is included because VM would be running and executing these bytecode instructions i.e. Chunk
+    uint8_t* ip;    //pointer of bytecode datatype to keep track of instruction line/ also known as program counter pointing to the next instruction which is supposed to be executed
 } VM;
 
 typedef enum
