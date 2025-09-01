a2d996981358cfe72d287d1c6948c372eeba07e3
priyanshupriyank04 <priyanshupriyank04@gmail.com>
2025-09-01 23:09:58 +0530

    tokenisation almost complete

--- scanner.c
+++ scanner.c
@@ -25,33 +25,127 @@ static bool isAtEnd()
     return *scanner.current == '\0'; // checks if the curr ptr of scanner has reached the null termination assuming that the source file has been added in form of C string
 }
 
+static char advance()
+{
+    scanner.current++;          // increment the scanner curr ptr by 1
+    return scanner.current[-1]; // return one back the character of scanner
+}
+
+static char peek()
+{
+    return *scanner.current;    //return the character being pointer by the curr ptr without updating the curr ptr
+}
+static bool match(char expected) // function to check for dual punctuation tokens
+{
+    if (isAtEnd())
+    {
+        return false; // if EOF return false
+    }
+
+    if (*scanner.current != expected) // if next character doesnt match return false
+    {
+        return false;
+    }
+
+    scanner.current++;
+    return true;
+}
 static Token makeToken(TokenType type)
 {
     Token token;
-    token.type = type;  //token will be created of the specified type
-    token.start = scanner.start;    //start ptr to be set at start ptr of scanner
-    token.length = (int)(scanner.current - scanner.start);  //curr ptr points one ahead of end of token so length will be curr - start ptr
-    token.line = scanner.line;  //token line is same as scanner line
+    token.type = type;                                     // token will be created of the specified type
+    token.start = scanner.start;                           // start ptr to be set at start ptr of scanner
+    token.length = (int)(scanner.current - scanner.start); // curr ptr points one ahead of end of token so length will be curr - start ptr
+    token.line = scanner.line;                             // token line is same as scanner line
 
     return token;
 }
 
-static Token errorToken(const char* message)
+static Token errorToken(const char *message)
 {
     Token token;
-    token.type = TOKEN_ERROR;   //token being created specially for error type
-    token.start = message;  //start ptr to be pointing at the message string being passed as the argument   
-    token.length = (int)strlen(message);    //length will be of the string message
-    token.line = scanner.line;      
+    token.type = TOKEN_ERROR;            // token being created specially for error type
+    token.start = message;               // start ptr to be pointing at the message string being passed as the argument
+    token.length = (int)strlen(message); // length will be of the string message
+    token.line = scanner.line;
 
     return token;
 }
+
+static void skipWhitespace()
+{
+    for (;;)
+    {
+        char c = peek(); // current character without updating the curr ptr
+        switch (c)
+        {
+        case ' ':      // whitespace character
+        case '\r':     // old style carriage return
+        case '\t':     // tab character
+            advance(); // skip the character
+            break;
+
+        case '\n':
+            scanner.line++;
+            advance();
+            break;
+        default:
+            return; // non skippable character so exit the loop
+        }
+    }
+}
+
 Token scantoken()
 {
+    skipWhitespace();
     scanner.start = scanner.current; // assign start pointer at the current ptr location in the file
     if (isAtEnd())
     {
         return makeToken(TOKEN_EOF); // if ptr reaches end of file generate eof token error and stops scanning for more tokens
+    }
+
+    char c = advance(); // function to grab one by one character from source and then move it to switch
+
+    switch (c)
+    { // different characters pointing to create different types of tokens
+    // single character tokens
+    case '(':
+        return makeToken(TOKEN_LEFT_PAREN);
+    case ')':
+        return makeToken(TOKEN_RIGHT_PAREN);
+    case '{':
+        return makeToken(TOKEN_LEFT_BRACE);
+    case '}':
+        return makeToken(TOKEN_RIGHT_BRACE);
+    case ';':
+        return makeToken(TOKEN_SEMICOLON);
+    case ',':
+        return makeToken(TOKEN_COMMA);
+    case '.':
+        return makeToken(TOKEN_DOT);
+    case '-':
+        return makeToken(TOKEN_MINUS);
+    case '+':
+        return makeToken(TOKEN_PLUS);
+    case '/':
+        return makeToken(TOKEN_SLASH);
+    case '*':
+        return makeToken(TOKEN_STAR);
+
+    // two character punctuation tokens
+    // uses a match function to check the next character if it matches then first option else it is a single character token
+    case '!':
+        return makeToken(
+            match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+    case '=':
+        return makeToken(
+            match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+    case '<':
+        return makeToken(
+            match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+    case '>':
+        return makeToken(
+            match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
     }
 
     return errorToken("Unexpected character."); // else manually report unexpected token error until other token handling is done
